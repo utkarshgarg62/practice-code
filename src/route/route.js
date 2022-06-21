49e45541c8300500cf8ae0dd3b8efccfc26b4a6e
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-06-21 15:06:57 +0530

    updated

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const AuthorController=require("../controller/authorController")
+const BlogController=require("../controller/blogController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-//router.post("/createUser", userController.createUser  )
+router.post("/author", AuthorController.createAuthor  )
+router.post("/createBlog", BlogController.createBlog)
+
 module.exports = router;
