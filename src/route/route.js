b884334c73ba897a3321738a14ea2a341dbb25c5
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-06-21 12:22:56 +0530

    day1

---
+++ src/route/route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+
+//router.post("/createUser", userController.createUser  )
+
+
+module.exports = router;
