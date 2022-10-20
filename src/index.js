48dccb0dcca0feacf36ce05f6f3dfeeebc698e88
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-10-20 09:58:03 +0530

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const HeadingComponentUsingFunction = () => {
+  return <h1>Heading 1 using functional Component</h1>
+}
+
+class HeadingComponentUsingClass extends React.Component{
+  render(){
+    return <h2>Heading 2 using Cal Component <CurrentTime/> </h2>
+  }
+}
+
+const CurrentTime = () => {
+  return <p> {+new Date()} </p>
+}
+
+const SubHeadingComponentUsingFunction = (props) => {
+  return <h3>{props.title}</h3>
+}
+
+class SubHeadingComponentUsingClass extends React.Component{
+  render(){
+    return <h3>{this.props.title}</h3>
+  }
+}
+
+setInterval(()=>{
+  root.render(<React.StrictMode>
+    <HeadingComponentUsingFunction />
+    <HeadingComponentUsingClass/>
+    <SubHeadingComponentUsingFunction title="It is sub heading using function" count="2"/> 
+    <SubHeadingComponentUsingClass title="It is sub heading using class"/> 
+  </React.StrictMode>);
+},1000)
