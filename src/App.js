7585c610afa3a3397a824a7f82acabe9717043de
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-10-21 14:24:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Counter from "./components/Counter"
 import Google from './components/Google'
+import List from './components/List'
 
 const App = () =>{
     return(
         <div className="main">
             <div className="google"><Google/></div>
             <div className="counter"><Counter/></div>
+            <div className="list"><List/></div>
         </div>
     )
 }
