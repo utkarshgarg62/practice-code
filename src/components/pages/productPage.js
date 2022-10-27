56b53a2515f752ba3c651422e40d11491141a6e4
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-10-27 19:08:28 +0530

    ecommerce

--- src/components/pages/productPage.js
+++ src/components/pages/productPage.js
@@ -1,7 +1,49 @@
-const ProductPage = () =>{
+import "./style/product-style1.css"
 
-    return <div>
-        <h1>Product Page</h1>
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const api = "https://fakestoreapi.com/products";
+
+const ProductPage = () => {
+  const [productList, setProductList] = useState();
+
+  useEffect(() => {
+    if (!productList) {
+      axios.get(api).then((response) => {
+        console.log(response.data);
+        setProductList(response.data);
+      });
+    }
+  });
+
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <div clasName="container">
+      {productList && productList.length > 0 && (
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Categeory</th>
+            <th>Rating</th>
+            <th>Price</th>
+          </tr>
+          {productList.map((product) => (
+            <tr>
+              {/* <td><a href={product.image}>Image</a></td> */}
+              <td><img src={product.image} alt="Girl in a jacket" width="50" height="60" /></td>
+              <td>{product.title}</td>
+              <td>{product.category}</td>
+              <td>{product.rating.rate}</td>
+              <td>₹ {product.price}</td>
+            </tr>
+          ))}
+        </table>
+      )}
+      </div>
     </div>
-}
-export default ProductPage
+  );
+};
+export default ProductPage;
