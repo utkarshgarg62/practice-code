f287f5741fc1ff13db5c5df9776e5197d9a9200b
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-10-27 09:23:49 +0530

    ecommerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Product from "./components/pages/productPage"
-import Payment from "./components/pages/paymentPage"
-import HomePage from "./components/pages/homePage"
-import ErrorPage from "./components/pages/errorPage"
-import CryptoCurrency from "./components/pages/cryptoCurrency"
+import NavBar from "./components/navBar";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="product" element={<Product/>}></Route>
-        <Route path="payment" element={<Payment/>}></Route>
-        <Route path="cryptocurrency" element={<CryptoCurrency/>}></Route>
-        <Route path="*" element={<ErrorPage/>}></Route>
-
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <NavBar />
+    </div>
   );
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
-import NavBar from './components/navBar'
+
+import Product from "./components/pages/productPage";
+import Payment from "./components/pages/paymentPage";
+import HomePage from "./components/pages/homePage";
+import ErrorPage from "./components/pages/errorPage";
+import CryptoCurrency from "./components/pages/cryptoCurrency";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <NavBar/>
-    <App />
-  </React.StrictMode>
+  <div>
+    <div>
+      <App />
+    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="payment" element={<Payment />}></Route>
+        <Route path="cryptocurrency" element={<CryptoCurrency />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
 );
